f581668939faa52384301d2aab314f6d8db897e8
Yunis Ibrahimov <yunis.ibrahimov.252@gmail.com>
2025-12-17 22:10:23 +0300

    Fix static file serving

--- server/static.ts
+++ server/static.ts
@@ -1,9 +1,18 @@
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import path from "path";
 import express, { type Express } from "express";
 import fs from "fs";
-import path from "path";
+
+// Fix for __dirname in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export function serveStatic(app: Express) {
+  // We need to go UP one level because we are in /server
+  // and the dist folder (public) is usually in /dist/public or similar
   const distPath = path.resolve(__dirname, "public");
+
   if (!fs.existsSync(distPath)) {
     throw new Error(
       `Could not find the build directory: ${distPath}, make sure to build the client first`,
